002b1e80f23e7f74fb38cf5cdc51a02fbe68bb43
Rafikahamad Jamakhane <rafiq4ahmad@gmail.com>
2024-07-13 14:03:01 +0530

    "unit tests"

--- src/test/java/com/example/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/example/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.userservice;
 
+import com.example.userservice.security.services.JpaRegisteredClientRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+
+import java.util.UUID;
 
 @SpringBootTest
 class UserServiceApplicationTests {
-
+    @Autowired
+    private JpaRegisteredClientRepository registeredClientRepository;
     @Test
     void contextLoads() {
     }
+
+    /*@Test
+    void storeRegisteredClientInDB() {
+        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("oidc-client")
+                .clientSecret("$2a$12$hN9A/jkxmYD3WorKQGKSBO2NHWIs2.vjxnnGadTYNWY95QXdQ7LQe")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("https://oauth.pstmn.io/v1/callback")
+                .postLogoutRedirectUri("https://oauth.pstmn.io/v1/callback")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .scope("ADMIN")
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+        registeredClientRepository.save(oidcClient);
+
+    }*/
 
 }
